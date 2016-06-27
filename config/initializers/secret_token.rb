92e61c4def8df0aea354836486604f1c06aec5fd
Jaime Moreno <j7isaac@gmail.com>
2016-06-27 20:47:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battlefront::Application.config.secret_key_base = '4650966beb8693bbd742747d33d690854da35009e7701f5ed0176ad7cd58c2dde808fc5ab31325e52a10cb3083c62f07ed44acdd121a278e8f2eafcb0b7b452f'
