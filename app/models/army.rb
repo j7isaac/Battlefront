5efb2dc9dc1507b0ffe95cf5fb1a14bb54e5e23a
Jaime Moreno <j7isaac@gmail.com>
2016-06-28 10:33:07 -0500

    Added comments to army and battle models

--- app/models/army.rb
+++ app/models/army.rb
@@ -1,6 +1,7 @@
 class Army < ActiveRecord::Base
   attr_reader :soldiers
 
+  # Create stats for each soldier type.
   def infantry_hash(id)
     { id: id, type: 'foot_soldier', health: 100, power: 75, defense: 15, luck: rand(10) }
   end
@@ -13,6 +14,7 @@ class Army < ActiveRecord::Base
     { id: id, type: 'knight', health: 100, power: 70, defense: 50, luck: rand(40) }
   end
 
+  # Create an array of soldiers based on the number for each type listed in the army object.
   def build_army
     @soldiers = []
     infantry.times { |id| @soldiers << infantry_hash(id) }
@@ -21,13 +23,20 @@ class Army < ActiveRecord::Base
     @soldiers
   end
 
+  # This method will be used to make the attacks.
   def attack(target)
+    # Select a random friendly soldier and assign it to a variable
     friendly = soldiers_alive.sample
+    # Select a random enemy soldier and assign it to a variable
     enemy = target.soldiers_alive.sample
+    # Use statistics from both attackers to generate a damage amount
     result = friendly[:power] + friendly[:luck] - enemy[:defense] - enemy[:luck]
+    # Use the damage amount to decrease the health of the target
+    # Amounts < 0 will not be processed
     target.soldiers_alive[target.soldiers_alive.index(enemy)][:health] -= result unless result < 0
   end
 
+  # Find all soldiers who still have health levels above 0
   def soldiers_alive
     soldiers.select { |soldier| soldier[:health] > 0 }
   end
--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,28 +1,40 @@
 class Battle < ActiveRecord::Base
   after_create :battle!
 
+  # The method will create a loop of attacks until there is a victor
   def battle!
+    # List the opposing armies
     battle_armies = [Army.find(army_a), Army.find(army_b)]
+    # Assign a random army to a variable
     @attack_initiator = battle_armies.sample
+    # Removed previous army from array
     battle_armies.delete(@attack_initiator)
+    # Assign the remaining army to a variable
     @other_army = battle_armies[0]
-
+    # Build soldier arrays for both armies
     @attack_initiator.build_army
     @other_army.build_army
 
+    # Create a loop of continuous attacks until a defeat has occurred
     until defeat_has_occurred?
+      # Attack initiator attacks other army first
       @attack_initiator.attack(@other_army)
+      # Break the loop if a defeat has occurred
       break if defeat_has_occurred?
+      # Other army counters
       @other_army.attack(@attack_initiator)
+      # Checks for defeat and if so breaks loop
       break if defeat_has_occurred?
     end
     victor
   end
 
+  # Checks if a defeat has occurred
   def defeat_has_occurred?
     @attack_initiator.soldiers_alive.empty? || @other_army.soldiers_alive.empty?
   end
 
+  # Display the army that survives the battle
   def victor
     return @attack_initiator if @attack_initiator.soldiers_alive.count > 0
     return @other_army if @other_army.soldiers_alive.count > 0
